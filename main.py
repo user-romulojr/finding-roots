30f34d0f53a02abd26c4612a785cc0a75330703f
Romulo <romueducation@gmail.com>
2024-04-19 23:03:59 +0800

    updates main lines of code

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Philippines!"
+    return render_template("app.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
